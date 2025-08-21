d8de22fc8149966ffb2e218c25b951c94ccd5c1f
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2025-08-21 23:29:07 +0700

    feat: setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 text-white min-h-screen">
+      <h1 className="text-3xl font-bold text-center py-8">
+        Scrollscape
+      </h1>
+    </div>
+  )
+}
+
+export default App
